144983e33b56016924c379b25f40de814ac88f7d
Gerrishon Sirere <gerryshon254@gmail.com>
2021-02-27 01:49:58 +0300

    Update __init__.py

--- src/quo/__init__.py
+++ src/quo/__init__.py
@@ -2,7 +2,8 @@
 Quo is a Python  based module for writing Command-Line Interface(CLI) applications. It improves programmer's productivity because it's easy to use and supports auto completion which means less time will be spent debugging. Simple to code, easy to learn, and does not come with needless baggage.
 
 """
-
+import importlib
+import os
 from .core import Argument
 from .core import BaseCommand
 from .core import Command
@@ -12,6 +13,7 @@ from .core import Group
 from .core import MultiCommand
 from .core import Option
 from .core import Parameter
+from .core import ShellDetectionFailure
 from .decorators import argument
 from .decorators import command
 from .decorators import confirmation_option
@@ -71,4 +73,23 @@ from .utilities import get_os_args
 from .utilities import get_text_stream
 from .utilities import open_file
 
-__version__ = "2021.02.dev6"
+
+
+def detect_shell(pid=None, max_depth=10):
+    name = os.name
+    try:
+        impl = importlib.import_module(".{}".format(name), __name__)
+    except ImportError:
+        message = "Shell detection not implemented for {0!r}".format(name)
+        raise RuntimeError(message)
+    try:
+        get_shell = impl.get_shell
+    except AttributeError:
+        raise RuntimeError("get_shell not implemented for {0!r}".format(name))
+    shell = get_shell(pid, max_depth=max_depth)
+    if shell:
+        return shell
+    raise ShellDetectionFailure()
+
+
+__version__ = "2021.02.dev7"
