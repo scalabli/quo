e9a51c03a493f3c52accde0a5004c6bcc38ea077
Gerrishon Sirere <gerryshon254@gmail.com>
2021-03-08 16:15:56 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,59 @@
+from pallets_sphinx_themes import get_version
+from pallets_sphinx_themes import ProjectLink
+
+import click._compat
+
+# compat until pallets-sphinx-themes is updated
+click._compat.text_type = str
+
+# Project --------------------------------------------------------------
+
+project = "Quo"
+copyright = "2021, Secretum Inc"
+author = "Secretum Inc"
+release, version = get_version("Quo", version_length=1)
+
+# General --------------------------------------------------------------
+
+master_doc = "index"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.log_cabinet",
+    "pallets_sphinx_themes",
+    "sphinx_issues",
+    "sphinx_tabs.tabs",
+]
+autodoc_typehints = "description"
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
+issues_github_path = "secretuminc/quo"
+
+# HTML -----------------------------------------------------------------
+
+html_theme = "quo"
+html_theme_options = {"index_sidebar_logo": False}
+html_context = {
+    "project_links": [
+        ProjectLink("Donate", "https://buymeacoffee.com/secretum"),
+        ProjectLink("PyPI Releases", "https://pypi.org/project/quo/"),
+        ProjectLink("Source Code", "https://github.com/pallets/click/"),
+        ProjectLink("Issue Tracker", "https://github.com/pallets/click/issues/"),
+        ProjectLink("Website", "https://palletsprojects.com/"),
+        ProjectLink("Twitter", "https://twitter.com/secretuminc"),
+        ProjectLink("Chat", "https://discord.gg/pallets"),
+    ]
+}
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
+html_static_path = ["_static"]
+html_favicon = "_static/quo.png"
+html_logo = "_static/click-logo-sidebar.png"
+html_title = f"Quo Documentation ({version})"
+html_show_sourcelink = False
+
+# LaTeX ----------------------------------------------------------------
+
+latex_documents = [(master_doc, f"Quo-{version}.tex", html_title, author, "manual")]
