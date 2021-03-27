e902b5930c4a1a5b0cff94d9ac94791a7facf569
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-03-28 00:32:57 +0300

    Update utils.py

--- src/quo/openapi/utils.py
+++ src/quo/openapi/utils.py
@@ -2,20 +2,20 @@ import http.client
 from enum import Enum
 from typing import Any, Dict, List, Optional, Sequence, Set, Tuple, Type, Union, cast
 
-from fastapi import routing
-from fastapi.datastructures import DefaultPlaceholder
-from fastapi.dependencies.models import Dependant
-from fastapi.dependencies.utils import get_flat_dependant, get_flat_params
-from fastapi.encoders import jsonable_encoder
-from fastapi.openapi.constants import (
+from quo.import routing
+from quo.datastructures import DefaultPlaceholder
+from quo.dependencies.models import Dependant
+from quo.dependencies.utils import get_flat_dependant, get_flat_params
+from quo.encoders import jsonable_encoder
+from quo.openapi.constants import (
     METHODS_WITH_BODY,
     REF_PREFIX,
     STATUS_CODES_WITH_NO_BODY,
 )
-from fastapi.openapi.models import OpenAPI
-from fastapi.params import Body, Param
-from fastapi.responses import Response
-from fastapi.utils import (
+from quo.openapi.models import OpenAPI
+from quo.params import Body, Param
+from quo.responses import Response
+from quo.utils import (
     deep_dict_update,
     generate_operation_id_for_path,
     get_model_definitions,
