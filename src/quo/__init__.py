88fc752d4c6d8c884701652a36b609442a062122
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-05-15 09:59:54 +0300

    Update __init__.py

--- src/quo/__init__.py
+++ src/quo/__init__.py
@@ -4,25 +4,8 @@ Quo is a Python based web framework and a toolkit for writing Command-Line Inter
 """
 
 
-from .applications import Quo as Quo
-from .background import BackgroundTasks as BackgroundTasks
-from .datastructures import UploadFile as UploadFile
 from .exceptions import HTTPException as HTTPException
-from .param_functions import Body as Body
-from .param_functions import Cookie as Cookie
-from .param_functions import Depends as Depends
-from .param_functions import File as File
-from .param_functions import Form as Form
-from .param_functions import Header as Header
-from .param_functions import Path as Path
-from .param_functions import Query as Query
-from .param_functions import Security as Security
 from .requests import Request as Request
-from .responses import Response as Response
-from .routing import APIRouter as APIRouter
-from .websockets import WebSocket as WebSocket
-from .websockets import WebSocketDisconnect as WebSocketDisconnect
-
 import importlib
 import os
 import subprocess
