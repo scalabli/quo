0f9273a1aa8c45169c4ae5ebbc9556e2d6867117
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-01 03:53:54 +0300

    Create autopswd.py

---
+++ src/quo/decorators/autopswd.py
@@ -0,0 +1,25 @@
+def autopswd(*param_decls, **kwargs):
+    """Add a ``--password`` option which prompts for a password, hiding
+    input and asking to enter the value again for confirmation.
+
+    :param param_decls: One or more option names. Defaults to the single
+        value ``"--password"``.
+    :param kwargs: Extra arguments are passed to :func:`option`.
+    
+    Example::
+
+    @quo.option('--password', prompt=True, confirmation_prompt=True,
+              hide_input=True)
+     def changeadmin(password):
+     pass
+    """
+
+    if not param_decls:
+        param_decls = ("--password",)
+
+    kwargs.setdefault("prompt", True)
+    kwargs.setdefault("confirmation_prompt", True)
+    kwargs.setdefault("hide_input", True)
+    return option(*param_decls, **kwargs)
+
+
