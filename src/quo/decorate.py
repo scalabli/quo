bf0dfc9b5ec6aae16af36ece1dd981af3f2bdcb4
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-01 11:50:50 +0300

    Update decorate.py

--- src/quo/decorate.py
+++ src/quo/decorate.py
@@ -175,4 +175,21 @@ def option(*param_decls, **attrs):
     return decorator
 
 
+def argument(*param_decls, **attrs):
+    """Attaches an argument to the command.  All positional arguments are
+    passed as parameter declarations to :class:`Argument`; all keyword
+    arguments are forwarded unchanged (except ``cls``).
+    This is equivalent to creating an :class:`Argument` instance manually
+    and attaching it to the :attr:`Command.params` list.
+
+    :param cls: the argument class to instantiate.  This defaults to
+                :class:`Argument`.
+    """
+
+    def decorator(f):
+        ArgumentClass = attrs.pop("cls", Argument)
+        _param_memo(f, ArgumentClass(param_decls, **attrs))
+        return f
+
+    return decorator
 
