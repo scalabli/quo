ce1b18691c235ce5840c5da2b247d01b1c4c87f4
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-01 10:30:23 +0300

    Update decorate.py

--- src/quo/decorate.py
+++ src/quo/decorate.py
@@ -14,8 +14,6 @@ from quo.decorators import autoconfirm
 from quo.decorators import autopswd
 from quo.decorators import autoversion
 from quo.decorators import autohelp
-from quo.decorators import option
-from quo.decorators import tether
 
 #Marks a callback as wanting to receive current context
 def contextualize(f):
@@ -153,6 +151,29 @@ def _param_memo(f, param):
 
 
 
+
+def option(*param_decls, **attrs):
+    """Attaches an option to the command.  All positional arguments are
+    passed as parameter declarations to :class:`Option`; all keyword
+    arguments are forwarded unchanged (except ``cls``).
+    This is equivalent to creating an :class:`Option` instance manually
+    and attaching it to the :attr:`Command.params` list.
+
+    :param cls: the option class to instantiate.  This defaults to
+                :class:`Option`.
+    """
+
+    def decorator(f):
+        # Issue 926, copy attrs, so pre-defined options can re-use the same cls=
+        option_attrs = attrs.copy()
+
+        if "help" in option_attrs:
+            option_attrs["help"] = inspect.cleandoc(option_attrs["help"])
+        OptionClass = option_attrs.pop("cls", Option)
+        _param_memo(f, OptionClass(param_decls, **option_attrs))
+        return f
+
+    return decorator
 
 
 
