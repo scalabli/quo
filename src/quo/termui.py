7e03c038a8eb1f00c90b4b9a5ecc730c19041616
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-15 19:52:00 +0300

    Update termui.py

--- src/quo/termui.py
+++ src/quo/termui.py
@@ -11,8 +11,8 @@ from .accordance import is_bytes
 from .accordance import isatty
 from .accordance import strip_ansi
 from .accordance import WIN
-from .exceptions import Abort
-from quo.outliers.usage_error import UsageError
+from quo.outliers.exceptions import Abort
+from quo.outliers.exceptions import UsageError
 from quo.context.current import resolve_color_default
 from .types import Choice
 from .types import convert_type
