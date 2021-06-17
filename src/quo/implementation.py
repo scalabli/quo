21c9329440af176ca3d1342edb14e76b8a7e762f
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-17 19:58:38 +0300

    Update implementation.py

--- src/quo/implementation.py
+++ src/quo/implementation.py
@@ -13,7 +13,7 @@ from .accordance import CYGWIN
 from .accordance import default_system_encoding
 from .accordance import isatty
 from .accordance import openstream
-from .accordance import strip_ansi
+from .accordance import strip_ansi_colors
 from .accordance import term_len
 from .accordance import WIN
 from quo.outliers.exceptions import QuoException
