6cfac06ad2674146e45f48633ec442f44c62be2c
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-17 16:03:50 +0300

    Update core.py

--- src/quo/core.py
+++ src/quo/core.py
@@ -21,10 +21,10 @@ from quo.context.current import push_context
 from .parser import _flag_needs_value
 from .parser import OptionParser
 from .parser import split_opt
-from .termui import confirm
-from .termui import prompt
-from .termui import flair
-from .termui import style
+from quo.termui import confirm
+from quo.termui import prompt
+from quo.termui import flair
+from quo.termui import style
 from .types import _NumberRangeBase
 from .types import BOOL
 from .types import convert_type
