11eaaed6080d70b0e2d89562488d8543d9db48f0
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-19 11:01:13 +0300

    Update decorate.py

--- src/quo/decorators/decorate.py
+++ src/quo/decorators/decorate.py
@@ -7,7 +7,7 @@ from functools import update_wrapper
 from quo.core import Argument
 from quo.core import Command
 from quo.core import Tether
-from quo.core import Option
+from quo.core import App
 from quo.context.current import currentcontext
 from quo.expediency.utilities import echo
 from quo.decorators import autoconfirm
@@ -110,7 +110,7 @@ def _make_command(f, name, attrs, cls):
 def command(name=None, cls=None, **attrs):
     r"""Creates a new :class:`Command` and uses the decorated function as
     callback.  This will also automatically attach all decorated
-    :func:`option`\s and :func:`argument`\s as parameters to the command.
+    :func:`app`\s and :func:`argument`\s as parameters to the command.
 
     The name of the command defaults to the name of the function with
     underscores replaced by dashes.  If you want to change that, you can
@@ -152,25 +152,25 @@ def _param_memo(f, param):
 
 
 
-def option(*param_decls, **attrs):
-    """Attaches an option to the command.  All positional arguments are
-    passed as parameter declarations to :class:`Option`; all keyword
+def app(*param_decls, **attrs):
+    """Attaches an app to the command.  All positional arguments are
+    passed as parameter declarations to :class:`App`; all keyword
     arguments are forwarded unchanged (except ``cls``).
-    This is equivalent to creating an :class:`Option` instance manually
+    This is equivalent to creating an :class:`App` instance manually
     and attaching it to the :attr:`Command.params` list.
 
-    :param cls: the option class to instantiate.  This defaults to
-                :class:`Option`.
+    :param cls: the app class to instantiate.  This defaults to
+                :class:`App`.
     """
 
     def decorator(f):
-        # Issue 926, copy attrs, so pre-defined options can re-use the same cls=
-        option_attrs = attrs.copy()
+        # Issue 926, copy attrs, so pre-defined apps can re-use the same cls=
+        app_attrs = attrs.copy()
 
-        if "help" in option_attrs:
-            option_attrs["help"] = inspect.cleandoc(option_attrs["help"])
-        OptionClass = option_attrs.pop("cls", Option)
-        _param_memo(f, OptionClass(param_decls, **option_attrs))
+        if "help" in app_attrs:
+            app_attrs["help"] = inspect.cleandoc(app_attrs["help"])
+        OptionClass = app_attrs.pop("cls", App)
+        _param_memo(f, OptionClass(param_decls, **app_attrs))
         return f
 
     return decorator
