eeb7dc1228e92b10943acd61a290ea03054c4240
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-20 10:02:25 +0300

    Update termui.py
    🎨 Fixing ansi color codes

--- src/quo/ui/termui.py
+++ src/quo/ui/termui.py
@@ -25,7 +25,7 @@ visible_prompt_func = input
 #American National Standard Institute colors
 
 ansi_color_codes = {
-    "black": 30,
+    "black": 16,
     "red": 31,
     "green": 32,
     "yellow": 33,
