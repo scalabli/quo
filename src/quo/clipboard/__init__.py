742fbd760e6e94c27f725bd3241b793774f76c5a
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-07-06 00:31:41 +0300

    Update __init__.py

--- src/quo/clipboard/__init__.py
+++ src/quo/clipboard/__init__.py
@@ -1,5 +1,5 @@
-from .base import Clipboard, ClipboardData, DummyClipboard, DynamicClipboard
-from .in_memory import InMemoryClipboard
+from quo.clipboard.base import Clipboard, ClipboardData, DummyClipboard, DynamicClipboard
+from quo.clipboard.in_memory import InMemoryClipboard
 
 # We are not importing `PyperclipClipboard` here, because it would require the
 # `pyperclip` module to be present.
