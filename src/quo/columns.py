75fac2f814346770b393548c35b50e1147aaa0a5
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-09-13 00:31:11 +0300

    Update columns.py

--- src/quo/columns.py
+++ src/quo/columns.py
@@ -3,21 +3,21 @@ from itertools import chain
 from operator import itemgetter
 from typing import Dict, Iterable, List, Optional, Tuple
 
-from .align import Align, AlignMethod
-from .terminal import Terminal, ConsoleOptions, RenderableType, RenderResult
-from .constrain import Constrain
-from .measure import Measurement
-from .padding import Padding, PaddingDimensions
-from .table import Table
-from .text import TextType
-from .jupyter import JupyterMixin
+from quo.align import Align, AlignMethod
+from quo.terminal import Terminal, ConsoleOptions, RenderableType, RenderResult
+from quo.constrain import Constrain
+from quo.width import Measurement
+from quo.padding import Padding, PaddingDimensions
+from quo.tabulate import Table
+from quo.text import TextType
+from quo.jupyter import JupyterMixin
 
 
 class Columns(JupyterMixin):
     """Display renderables in neat columns.
 
     Args:
-        renderables (Iterable[RenderableType]): Any number of Rich renderables (including str).
+        renderables (Iterable[RenderableType]): Any number of Quo renderables (including str).
         width (int, optional): The desired width of the columns, or None to auto detect. Defaults to None.
         padding (PaddingDimensions, optional): Optional padding around cells. Defaults to (0, 1).
         expand (bool, optional): Expand columns to full width. Defaults to False.
@@ -59,7 +59,7 @@ class Columns(JupyterMixin):
         """
         self.renderables.append(renderable)
 
-    def __rich_console__(
+    def __quo_console__(
         self, console: Terminal, options: ConsoleOptions
     ) -> RenderResult:
         render_str = console.render_str
