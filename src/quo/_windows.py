b0f010031f95edfd9b2aeaea4d00dd58efc5c32b
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-09-13 00:12:47 +0300

    Update _windows.py

--- src/quo/_windows.py
+++ src/quo/_windows.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     import platform
 
     features = get_windows_console_features()
-    from rich import print
+    from quo import print
 
     print(f'platform="{platform.system()}"')
     print(repr(features))
