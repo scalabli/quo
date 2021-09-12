4a849bfb15924b0c7a57f5defdbfc190a6436b6f
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-09-13 00:04:41 +0300

    Update _extension.py

--- src/quo/_extension.py
+++ src/quo/_extension.py
@@ -3,8 +3,8 @@ from typing import Any
 
 def load_ipython_extension(ip: Any) -> None:  # pragma: no cover
     # prevent circular import
-    from rich.pretty import install
-    from rich.traceback import install as tr_install
+    from quo.pretty import install
+    from quo.traceback import install as tr_install
 
     install()
     tr_install()
