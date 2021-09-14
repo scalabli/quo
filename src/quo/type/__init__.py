9d69742a39ec91e45e3b551a10b3c1c6981a41a2
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-09-14 14:11:56 +0300

    Create __init__.py

---
+++ src/quo/type/__init__.py
@@ -0,0 +1,16 @@
+from quo.types import (
+BOOL,
+              Choice,
+              DateTime,
+              File,
+              FLOAT,
+              FloatRange,
+              INT,
+              IntRange,
+              ParamType,
+              Path,
+              STRING,
+              Tuple,
+              UNPROCESSED,
+              UUID
+              )
