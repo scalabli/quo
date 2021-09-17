80d0ea87cbbbaf9b53bae00075c6c6c7c6a38696
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-09-17 20:26:39 +0300

    Update termui.py

--- src/quo/i_o/termui.py
+++ src/quo/i_o/termui.py
@@ -26,8 +26,6 @@ from quo.expediency import inscribe, LazyFile
 
 insert = input
 
-_console: Optional["Console"] = None
-
 
 
 def hidden_prompt_func(prompt):
@@ -54,137 +52,10 @@ def _format_default(default):
     return default
 
 ##################################################################
-def prompiht(
-    text,
-    default=None,
-    hide=False,
-    autoconfirm=False,
-    type=None,
-    value_proc=None,
-    prompt_suffix=":> ",
-    show_default=True,
-    err=False,
-    show_choices=True,
-):
 
-    """Prompts a user for input.  This is a convenience function that can be used to prompt a user for input later.
-
-    If the user aborts the input by sending a interrupt signal, this  function will catch it and raise a :exc:`Abort` exception.
-
-    :param text: the text to show for the prompt.
-    :param default: the default value to use if no input happens.  If this  is not given it will prompt until it's aborted.
-    :param hide_input: if this is set to true then the input value will  be hidden.
-    :param autoconfirm: asks for confirmation for the value.
-    :param type: the type to use to check the value against.
-    :param value_proc: if this parameter is provided it's a function that is invoked instead of the type conversion to convert a value.
-    :param prompt_suffix: a suffix that should be added to the prompt.
-    :param show_default: shows or hides the default value in the prompt.
-    :param err: if set to true the file defaults to ``stderr`` instead of ``stdout``, the same as with echo.
-    :param show_choices: Show or hide choices if the passed type is a Choice. For example if type is a Choice of either day or week, show_choices is true and text is "Group by" then the  prompt will be "Group by (day, week): ".
-
-    """
-    result = None
-
-    def prompt_func(text):
-        f = hidden_prompt_func if hide else visible_prompt_func
-        try:
-            # Write the prompt separately so that we get nice
-            # coloring through colorama on Windows
-            inscribe(text, nl=False, err=err)
-            return f("")
-        except (KeyboardInterrupt, EOFError):
-            # getpass doesn't print a newline if the user aborts input with ^C.
-            # Allegedly this behavior is inherited from getpass(3).
-            # A doc bug has been filed at https://bugs.python.org/issue24711
-            if hide:
-                inscribe(None, err=err)
-            raise Abort()
-
-    if value_proc is None:
-        value_proc = convert_type(type, default)
-
-    prompt = _build_prompt(
-        text, prompt_suffix, show_default, default, show_choices, type
-    )
-
-    while 1:
-        while 1:
-            value = prompt_func(prompt)
-            if value:
-                break
-            elif default is not None:
-                value = default
-                break
-        try:
-            result = value_proc(value)
-        except UsageError as e:
-            if hide:
-                inscribe("Error: the value you entered was invalid", err=err)
-            else:
-                inscribe(f"Error: {e.message}", err=err)  # noqa: B306
-            continue
-        if not autoconfirm:
-            return result
-        while 1:
-            value2 = prompt_func("Repeat for confirmation: ")
-            if value2:
-                break
-        if value == value2:
-            return result
-        echo("Error: the two entered values do not match", err=err)
-##############################################
-
-def get_terminal() -> "Terminal":
-    """Get a global :class:`~quo.terminal.Terminal` instance. This function is used when Rich requires a Console,               and hasn't been explicitly given one.                               Returns:                                   Console: A console instance.         """
-    global _console
-    if _console is None:
-        from quo.terminal import Terminal
-        _console = Terminal()
-    return _console
 
 #############################
 
-def confirmij(
-        text, default=False, abort=False, prompt_suffix=":>", show_default=True, err=False
-):
-    """Prompts for confirmation (yes/no question).
-
-    If the user aborts the input by sending a interrupt signal this
-    function will catch it and raise a :exc:`Abort` exception.
-
-    :param text: the question to ask.
-    :param default: the default for the prompt.
-    :param abort: if this is set to `True` a negative answer aborts the
-                  exception by raising :exc:`Abort`.
-    :param prompt_suffix: a suffix that should be added to the prompt.
-    :param show_default: shows or hides the default value in the prompt.
-    :param err: if set to true the file defaults to ``stderr`` instead of
-                ``stdout``, the same as with echo.
-    """
-    prompt = _build_prompt(
-        text, prompt_suffix, show_default, "Yes/no" if default else "yes/No"
-    )
-    while 1:
-        try:
-            # Write the prompt separately so that we get nice
-            # coloring through colorama on Windows
-            echo(prompt, nl=False, err=err, fg="cyan")
-            value = insert("").lower().strip()
-        except (KeyboardInterrupt, EOFError):
-            raise Abort()
-        if value in ("y", "yes"):
-            rv = True
-        elif value in ("n", "no"):
-            rv = False
-        elif default is not None and value == "":
-            rv = default
-        else:
-            echo("Error: invalid input", bg="yellow", fg="black", err=err)
-            continue
-        break
-    if abort and not rv:
-        raise Abort()
-    return rv
 
 ##########################################################################
 
