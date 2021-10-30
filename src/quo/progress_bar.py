010c2432672fd4a7067b8a46007f5f48ecf851dc
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-10-30 13:37:42 +0300

    Update progress_bar.py

--- src/quo/progress_bar.py
+++ src/quo/progress_bar.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 from time import monotonic
 from typing import Iterable, List, Optional
 
-from .color import Color, blend_rgb
+from quo.color.color import Color, blend_rgb
 from .color_triplet import ColorTriplet
 from .console import Console, ConsoleOptions, RenderResult
 from .jupyter import JupyterMixin
