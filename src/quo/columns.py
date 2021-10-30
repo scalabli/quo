d6f5cb8824ca1b3d5cbab83ffc4d82e6f6b44f30
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-10-30 16:47:39 +0300

    Update columns.py

--- src/quo/columns.py
+++ src/quo/columns.py
@@ -9,7 +9,7 @@ from .constrain import Constrain
 from .measure import Measurement
 from .padding import Padding, PaddingDimensions
 from .table import Table
-from .text import TextType
+from quo.text.text import TextType
 from .jupyter import JupyterMixin
 
 
