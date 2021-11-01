867eaec06428e97671666e4a5bc61f8e81265c2e
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-01 18:38:13 +0300

    Update interpose.py

--- src/quo/interpose.py
+++ src/quo/interpose.py
@@ -2,7 +2,8 @@ from quo.expediency import inscribe
 
 
 _interpose = None
-def interpose(inscribe=False):
+def interpose(inscribe: bool = False) -> str:
+  
     """Fetches a single character from the terminal and returns it.  This
     will always return a unicode character and under certain rare
     circumstances this might return more than one character.  The
@@ -17,6 +18,7 @@ def interpose(inscribe=False):
     :param inscribe: if set to `True`, the character read will also show up on
                  the terminal.  The default is to not show it.
     """
+    global _interpose
     f = _interpose
     if f is None:
         from quo.implementation import interpose as f
