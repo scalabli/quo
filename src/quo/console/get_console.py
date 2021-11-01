56fc6c50ae20e3074569988c34182ada29272b63
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-01 22:31:23 +0300

    Create get_console.py

---
+++ src/quo/console/get_console.py
@@ -0,0 +1,14 @@
+def get_console() -> "Console":
+    """Get a global :class:`~rich.console.Console` instance. This function is used when Rich requires a Console,
+    and hasn't been explicitly given one.
+
+    Returns:
+        Console: A console instance.
+    """
+    global _console
+    if _console is None:
+        from .console import Console
+
+        _console = Console()
+
+    return _console
