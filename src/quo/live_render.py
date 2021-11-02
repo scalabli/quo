dc4838b12913d3287ff431a6b043564025bf6991
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-02 17:33:58 +0300

    Update live_render.py

--- src/quo/live_render.py
+++ src/quo/live_render.py
@@ -8,7 +8,7 @@ else:
 
 
 from ._loop import loop_last
-from .console import Console, ConsoleOptions, RenderableType, RenderResult
+from quo.console.console import Console, ConsoleOptions, RenderableType, RenderResult
 from .control import Control
 from .segment import ControlType, Segment
 from .style import StyleType
