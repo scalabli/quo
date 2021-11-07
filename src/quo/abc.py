c49a4e64890c014f1257067b2c73c8cc3ba98120
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-07 22:12:06 +0300

    Update abc.py

--- src/quo/abc.py
+++ src/quo/abc.py
@@ -2,19 +2,19 @@ from abc import ABC
 
 
 class RichRenderable(ABC):
-    """An abstract base class for Rich renderables.
+    """An abstract base class for renderables.
 
     Note that there is no need to extend this class, the intended use is to check if an
-    object supports the Rich renderable protocol. For example::
+    object supports the renderables. For example::
 
         if isinstance(my_object, RichRenderable):
-            console.print(my_object)
+            console.echo(my_object)
 
     """
 
     @classmethod
     def __subclasshook__(cls, other: type) -> bool:
-        """Check if this class supports the rich render protocol."""
+        """Check if this class supports the render protocol."""
         return hasattr(other, "__rich_console__") or hasattr(other, "__rich__")
 
 
