d24b23c68b00b585d681e49dc56395c21e47a327
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-07 22:41:58 +0300

    Update logging.py

--- src/quo/logging.py
+++ src/quo/logging.py
@@ -11,6 +11,21 @@ from .highlighter import Highlighter, ReprHighlighter
 from .text import Text
 from .traceback import Traceback
 
+_console: Optional["Console"] = None
+
+def _get_console() -> "Console":
+    """Get a global :class:`~quo.console.Console` instance. This function is used when Quo requires a Console,
+    and hasn't been explicitly given one.
+
+    Returns:
+        Console: A console instance.
+    """
+    global _console
+    if _console is None:
+
+        _console = Console()
+
+    return _console
 
 class RichHandler(Handler):
     """A logging handler that renders output with Rich. The time / level / message and file are displayed in columns.
