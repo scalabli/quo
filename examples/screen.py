940b7081537baa55e820414069c6a2c65ed2779a
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-16 10:41:37 +0300

    Update screen.py

--- examples/screen.py
+++ examples/screen.py
@@ -2,17 +2,12 @@
 Demonstration of Console.screen() 
 """
 from time import sleep
-from quo.keys import KeyBinder
-from quo.align import Align
-from quo.console import Console
+from quo import Align, Console
 from quo.panel import Panel
 
 console = Console()
-kb = KeyBinder()
-@kb.add("ctrl-c")
-def q(quit):
-    quit.app.exit()
+
 with console.screen(style="bold white on red") as screen:
-    text = Align.center("[blink]Press Ctrl-C to quit[/blink]", vertical="middle")
+    text = Align.center("[blink] [/blink]", vertical="middle")
     screen.update(Panel(text))
     sleep(10)
