f2e872f741ca6ffbf6fcf31d65c787dd52070f1c
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 14:39:30 +0300

    Update layout.py

--- src/quo/layout.py
+++ src/quo/layout.py
@@ -416,30 +416,3 @@ class Layout:
             for layout_row in layout_lines:
                 yield from layout_row
                 yield new_line
-
-
-if __name__ == "__main__":
-    from quo import Console
-
-    console = Console()
-    layout = Layout()
-
-    layout.split_column(
-        Layout(name="header", size=3),
-        Layout(ratio=1, name="main"),
-        Layout(size=10, name="footer"),
-    )
-
-    layout["main"].split_row(Layout(name="side"), Layout(name="body", ratio=2))
-
-    layout["body"].split_row(Layout(name="content", ratio=2), Layout(name="s2"))
-
-    layout["s2"].split_column(
-        Layout(name="top"), Layout(name="middle"), Layout(name="bottom")
-    )
-
-    layout["side"].split_column(Layout(layout.tree, name="left1"), Layout(name="left2"))
-
-    layout["content"].update("foo")
-
-    console.echo(layout)
