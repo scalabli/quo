7ff02f0d8b02002b9db6808b40ae154f70206cf3
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 14:46:52 +0300

    Update color.py

--- src/quo/color/color.py
+++ src/quo/color/color.py
@@ -341,32 +341,3 @@ def blend_rgb(
         int(b1 + (b2 - b1) * cross_fade),
     )
     return new_color
-
-
-if __name__ == "__main__":  # pragma: no cover
-
-    from .console import Console
-    from .table import Table
-    from quo.text.text import Text
-
-    console = Console()
-
-    table = Table(show_footer=False, show_edge=True)
-    table.add_column("Color", width=10, overflow="ellipsis")
-    table.add_column("Number", justify="right", style="yellow")
-    table.add_column("Name", style="green")
-    table.add_column("Hex", style="blue")
-    table.add_column("RGB", style="magenta")
-
-    colors = sorted((v, k) for k, v in ANSI_COLOR_NAMES.items())
-    for color_number, name in colors:
-        color_cell = Text(" " * 10, style=f"on {name}")
-        if color_number < 16:
-            table.add_row(color_cell, f"{color_number}", Text(f'"{name}"'))
-        else:
-            color = EIGHT_BIT_PALETTE[color_number]  # type: ignore
-            table.add_row(
-                color_cell, str(color_number), Text(f'"{name}"'), color.hex, color.rgb
-            )
-
-    console.print(table)
