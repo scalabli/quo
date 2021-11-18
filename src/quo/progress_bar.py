47915d937bf2cfffb19990a3d2b197ae6cfbf9fa
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 14:12:00 +0300

    Update progress_bar.py

--- src/quo/progress_bar.py
+++ src/quo/progress_bar.py
@@ -199,18 +199,3 @@ class ProgressBar(JupyterMixin):
             else Measurement(4, options.max_width)
         )
 
-
-if __name__ == "__main__":  # pragma: no cover
-    console = Console()
-    bar = ProgressBar(width=50, total=100)
-
-    import time
-
-    console.show_cursor(False)
-    for n in range(0, 101, 1):
-        bar.update(n)
-        console.echo(bar)
-        console.file.write("\r")
-        time.sleep(0.05)
-    console.show_cursor(True)
-    console.echo()
