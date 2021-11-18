bec8bc99ba01f0ae3b8305a39c3d660f752c57b0
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 14:41:57 +0300

    Update highlighter.py

--- src/quo/highlighter.py
+++ src/quo/highlighter.py
@@ -113,34 +113,3 @@ class JSONHighlighter(RegexHighlighter):
         ),
         r"(?<![\\\w])(?P<key>b?\".*?(?<!\\)\")\:",
     ]
-
-
-if __name__ == "__main__":  # pragma: no cover
-    from quo.console import Console
-
-    console = Console()
-    console.print("[bold green]hello world![/bold green]")
-    console.print("'[bold green]hello world![/bold green]'")
-
-    console.print(" /foo")
-    console.print("/foo/")
-    console.print("/foo/bar")
-    console.print("foo/bar/baz")
-
-    console.print("/foo/bar/baz?foo=bar+egg&egg=baz")
-    console.print("/foo/bar/baz/")
-    console.print("/foo/bar/baz/egg")
-    console.print("/foo/bar/baz/egg.py")
-    console.print("/foo/bar/baz/egg.py word")
-    console.print(" /foo/bar/baz/egg.py word")
-    console.print("foo /foo/bar/baz/egg.py word")
-    console.print("foo /foo/bar/ba._++z/egg+.py word")
-    console.print("https://example.org?foo=bar#header")
-
-    console.print(1234567.34)
-    console.print(1 / 2)
-    console.print(-1 / 123123123123)
-
-    console.print(
-        "127.0.1.1 bar 192.168.1.4 2001:0db8:85a3:0000:0000:8a2e:0370:7334 foo"
-    )
