688b75d22cc4fab4a4f78ece5bd4085dd7106c4c
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 13:50:21 +0300

    Update status.py

--- src/quo/status.py
+++ src/quo/status.py
@@ -105,28 +105,3 @@ class Status(JupyterMixin):
     ) -> None:
         self.stop()
 
-
-if __name__ == "__main__":  # pragma: no cover
-
-    from time import sleep
-
-    from quo.console import Console
-
-    console = Console()
-    with console.status("[magenta]Covid detector booting up") as status:
-        sleep(3)
-        console.log("Importing advanced AI")
-        sleep(3)
-        console.log("Advanced Covid AI Ready")
-        sleep(3)
-        status.update(status="[bold blue] Scanning for Covid", spinner="earth")
-        sleep(3)
-        console.log("Found 10,000,000,000 copies of Covid32.exe")
-        sleep(3)
-        status.update(
-            status="[bold red]Moving Covid32.exe to Trash",
-            spinner="bouncingBall",
-            spinner_style="yellow",
-        )
-        sleep(5)
-    console.echo("[bold green]Covid deleted successfully")
