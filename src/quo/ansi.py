2fac529364e1cfe9092cc48bbd7a7f5c8023c04b
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-18 14:49:11 +0300

    Update ansi.py

--- src/quo/ansi.py
+++ src/quo/ansi.py
@@ -196,33 +196,3 @@ class AnsiDecoder:
                                 )
 
         return text
-
-
-if __name__ == "__main__":  # pragma: no cover
-    import pty
-    import io
-    import os
-    import sys
-
-    decoder = AnsiDecoder()
-
-    stdout = io.BytesIO()
-
-    def read(fd: int) -> bytes:
-        data = os.read(fd, 1024)
-        stdout.write(data)
-        return data
-
-    pty.spawn(sys.argv[1:], read)
-
-    from .console import Console
-
-    console = Console(record=True)
-
-    stdout_result = stdout.getvalue().decode("utf-8")
-    print(stdout_result)
-
-    for line in decoder.decode(stdout_result):
-        console.print(line)
-
-    console.save_html("stdout.html")
