36500d80c72030506f872b1b1d8ac099982b23b2
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-11-20 02:08:26 +0300

    Update __init__.py

--- src/quo/__init__.py
+++ src/quo/__init__.py
@@ -90,49 +90,5 @@ from quo.i_o import (
 #from quo.widget import TextArea, Frame
 
 
-#    """Get a global :class:`~quo.console.Console` instance. This function is used when Rich requires a Console,
-#    and hasn't been explicitly given one.
-
-#    Returns:
- #       Console: A console instance.
-#    """
-#    """Reconfigures the global console by replacing it with another.
-
-#    Args:
-#        console (Console): Replacement console instance.
- #   """
- #   """Pretty prints JSON. Output will be valid JSON.
-
-#    Args:
- #       json (str): A string containing JSON.
-#        data (Any): If json is not supplied, then encode this data.
- #       indent (int, optional): Number of spaces to indent. Defaults to 2.
-   #     highlight (bool, optional): Enable highlighting of output: Defaults to True.
-#    """
-
- #   get_console().print_json(json, data=data, indent=indent, highlight=highlight)
-
-
-#    """Inspect any Python object.
-
-#    * inspect(<OBJECT>) to see summaris=True) to see methods.
-#    * inspect(<OBJECT>, help=True) to see full (non-abbreviated) help.
-#    * inspect(<OBJECT>, private=True) to see private attributes (single underscore).
- #   * inspect(<OBJECT>, dunder=True) to see attributes beginning with double underscore.
-#    * inspect(<OBJECT>, all=True) to see all attributes.
-
-#    Args:
-#        obj (Any): An object to inspect.
-#        title (str, optional): Title to display over inspect result, or None use type. Defaults to None.
-#        help (bool, optional): Show full help text rather than just first paragraph. Defaults to False.
-#        methods (bool, optional): Enable inspection of callables. Defaults to False.
- #       docs (bool, optional): Also render doc strings. Defaults to True.
- #       private (bool, optional): Show private attributes (beginning with underscore). Defaults to False.
- #       dunder (bool, optional): Show attributes starting with double underscore. Defaults to False.
-#        sort (bool, optional): Sort attributes alphabetically. Defaults to True.
- #       all (bool, optional): Show all attributes. Defaults to False.
-#        value (bool, optional): Pretty print value#. Defaults to True.
-
-
 
 __version__ = "2021.6"
