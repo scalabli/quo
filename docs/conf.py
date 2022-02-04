3b758eaa88bcd59ef6e4b80c64c1e52c66870841
Gerrishon Sirere <s.gerrishon@gmail.com>
2022-02-04 14:10:52 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.graphviz", "sphinx_rtd_dark_mode
 # templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = ".rst"
+source_suffix = [".rst", ".md"]
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
